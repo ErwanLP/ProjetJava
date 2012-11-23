a47ed36d3bf4fa24e5a3e102805f5d832bfef658
ErwanLP <erwan.le-poder@isep.fr>
2012-11-23 22:11:00 +0100

    init

--- src/projetjava/ProjetJava.java
+++ src/projetjava/ProjetJava.java
@@ -1,18 +1,62 @@
 package projetjava;
 
+import java.util.Scanner;
+
 /*import java.util.Random;*/
-
-
 /*int valeur = 1 + r.nextInt(37 - 1);*/
 public class ProjetJava {
 
     public static void main(String[] args) {
-        int n = 12;
-        int m = 12;
-        int[][] grille = new int[10][10];
-        char[][] tabVide = new char[12][12];
-        GenereTableauVide(tabVide, n, m);
-        AfficheGrille(grille);
+        /*    int n = 12;
+         int m = 12;
+         int[][] grille = new int[10][10];
+         char[][] tabVide = new char[12][12];
+         GenereTableauVide(tabVide, n, m);
+         AfficheGrille(grille);*/
+        menuPrincipale();
+
+    }
+
+    public static void menuPrincipale() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("STREAM");
+        System.out.println("Par Erwan Le Poder et Florian Migot");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("MENU");
+        System.out.println("1-jouer");
+        System.out.println("2-deux");
+        System.out.println("3-trois");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Votre choix ?");
+        int choix = sc.nextInt();
+        String monthString;
+        switch (choix) {
+            case 1:
+                jouer();
+                break;
+            case 2:
+                //deux();
+                break;
+            case 3:
+                //trois();
+                break;
+
+            default:
+                menuPrincipale();
+                break;
+        }
+
+    }
+    
+    
+    
+    public static void jouer(){
+        
+        
+        
+        
+        
     }
 
     public static void GenereTableauVide(char[][] tabVide, int n, int m) {
